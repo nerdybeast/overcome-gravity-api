25d9f21ddf236f36476e83e2d7577eb835654f7f
Michael Penrod <therealnerdybeast@gmail.com>
2019-08-07 22:41:17 -0500

    Ditched nodemon in favor of running the project via vscode launch config

--- src/local-dev.ts
+++
@@ -1,9 +0,0 @@
-/**
- * We are using dotenv when developing locally to populate process.env with the values from our .env file.
- * Heroku automatically does this when we deploy so dotenv is not needed in that case which is why it's been
- * added to this project as a dev dependency.
- */
-import dotenv from 'dotenv';
-dotenv.config();
-
-import './main';
